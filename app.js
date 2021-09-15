e11fb3c01fe59e5ec1414a4223587ecb8d22121e
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2021-09-15 12:37:35 -0300

    make it possible to add new items to todo list

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 
+const formAddTodo = document.querySelector('.form-add-todo')
+const todosContainer = document.querySelector('.todos-container')
+
+const generateTodo = message => `
+<li class="list-group-item d-flex justify-content-between align-items-center">
+  <span>${message}</span>
+  <i class="far fa-trash-alt delete"></i>
+</li>
+`
+
+formAddTodo.addEventListener('submit', event => {
+  event.preventDefault()
+
+  const inputValue = event.target.add.value.trim()
+  if (inputValue.length) {
+    todosContainer.innerHTML += generateTodo(event.target.add.value)
+
+    event.target.reset()
+  }
+})

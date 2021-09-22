7035822702bc8bc8cfde4adaaa671870cf470bc9
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2021-09-22 15:33:10 -0300

    implements search and delete functionalities

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const formAddTodo = document.querySelector('.form-add-todo')
 const todosContainer = document.querySelector('.todos-container')
+const formSearchTodo = document.querySelector('.form-search')
 
 const generateTodo = message => `
 <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -17,5 +18,37 @@ formAddTodo.addEventListener('submit', event => {
     todosContainer.innerHTML += generateTodo(event.target.add.value)
 
     event.target.reset()
+  }
+})
+
+formSearchTodo.addEventListener('submit', event => {
+  event.preventDefault()
+})
+
+formSearchTodo.search.addEventListener('input', event => {
+  const lis = Array.from(todosContainer.children)
+  const inputValue = event.target.value
+  const hideLi = li => {
+    li.setAttribute('class', 'd-none')
+  }
+  const resetLiClasses = li => {
+    li.setAttribute('class', 'list-group-item d-flex justify-content-between align-items-center')
+  }
+  
+  lis.forEach(li => {
+
+    const isTodoPresent = li.querySelector('span').textContent.includes(inputValue)
+    
+    !isTodoPresent ? hideLi(li) : resetLiClasses(li)
+  
+  })
+})
+
+todosContainer.addEventListener('click', event => {
+  const clickedElement = event.target
+  const isClickedElementTrashIcon = Array.from(clickedElement.classList).includes('delete')
+
+  if (isClickedElementTrashIcon) {
+    clickedElement.parentElement.remove()
   }
 })

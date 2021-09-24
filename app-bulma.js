3c96fd61c02bb54ac0557171ecd992e0f8bbacea
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2021-09-24 19:24:08 -0300

    finish refactoring bulma app code

--- app-bulma.js
+++ app-bulma.js
@@ -3,40 +3,59 @@ const todosContainer = document.querySelector('#todos-container')
 const formSearchTodo = document.querySelector('.form-search input')
 
 const generateTodoHTML = todoContent => `
-<li class="message has-background-warning has-text-dark my-1 p-3 is-flex is-justify-content-space-between is-align-items-center">
-  <span>${todoContent}</span><i class="fas fa-trash-alt is-clickable"></i>
+<li data-todo-content="${todoContent}" class="message has-background-warning has-text-dark my-1 p-3 is-flex is-justify-content-space-between is-align-items-center">
+  <span>${todoContent}</span><i class="fas fa-trash-alt is-clickable delete-item" data-todo-content="${todoContent}"></i>
 </li>
 `
 
-formAddTodo.addEventListener('submit', event => {
+const handleAddTodo = event => {
   event.preventDefault()
-  const inputValue = event.target.add.value
-
-  todosContainer.innerHTML += generateTodoHTML(inputValue)
-  event.target.reset()
-})
-
-todosContainer.addEventListener('click', event => {
-  const clickedElement = event.target
-
-  if (Array.from(clickedElement.classList).includes('fa-trash-alt')) {
-    clickedElement.parentElement.remove()
+  const inputValue = event.target.add.value.trim()
+  const isThereAnInput = inputValue.length
+  
+  if (isThereAnInput) {
+    todosContainer.innerHTML += generateTodoHTML(inputValue)
   }
-})
+  event.target.reset()
+}
 
-formSearchTodo.addEventListener('input', event => {
+const removeLi = element => {document
+  .querySelector(`[data-todo-content="${element.dataset.todoContent}"]`)
+  .remove()}
+
+const handleTodoDeletion = event => {
+  const clickedElement = event.target
+  const isClickedElementTrashIcon = Array.from(clickedElement.classList)
+  .includes('delete-item')
+  
+  if (isClickedElementTrashIcon) {
+    removeLi(clickedElement)
+  }
+  }
+
+const hideMismatchingTodos =  (array, todoContent) => {
+  array
+  .filter(todo => !todo.textContent.toLowerCase().includes(todoContent))
+  .forEach(todo => {
+    todo.classList.add('is-hidden')
+  })
+}
+const showMatchingTodos = (array, todoContent) => {
+  array
+  .filter(todo => todo.textContent.toLowerCase().includes(todoContent))
+  .forEach(todo => {
+    todo.classList.remove('is-hidden')
+  })
+}
+
+const handleTodoSearches = event => {
   const inputValue = event.target.value.trim().toLowerCase()
+  const todosArray = Array.from(todosContainer.children)
 
-  Array.from(todosContainer.children)
-    .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
-    .forEach(todo => {
-      todo.classList.add('is-hidden')
-    })
+  hideMismatchingTodos(todosArray, inputValue)
+  showMatchingTodos(todosArray, inputValue)
+}
 
-  Array.from(todosContainer.children)
-    .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
-    .forEach(todo => {
-      todo.classList.remove('is-hidden')
-    })
-
-})
+formAddTodo.addEventListener('submit', handleAddTodo)
+todosContainer.addEventListener('click', handleTodoDeletion)
+formSearchTodo.addEventListener('input', handleTodoSearches)

b9815f797f8134a2d736666b17339863b5ef3747
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2021-09-24 16:17:39 -0300

    refactor code for add todo feature

--- app-bulma.js
+++ app-bulma.js
@@ -2,14 +2,17 @@ const formAddTodo = document.querySelector('#form-add-todo')
 const todosContainer = document.querySelector('#todos-container')
 const formSearchTodo = document.querySelector('.form-search input')
 
+const generateTodoHTML = todoContent => `
+<li class="message has-background-warning has-text-dark my-1 p-3 is-flex is-justify-content-space-between is-align-items-center">
+  <span>${todoContent}</span><i class="fas fa-trash-alt is-clickable"></i>
+</li>
+`
+
 formAddTodo.addEventListener('submit', event => {
   event.preventDefault()
+  const inputValue = event.target.add.value
 
-  todosContainer.innerHTML += `
-    <li class="message has-background-warning has-text-dark my-1 p-3 is-flex is-justify-content-space-between is-align-items-center">
-      <span>${event.target.add.value}</span><i class="fas fa-trash-alt is-clickable"></i>
-    </li>
-  `
+  todosContainer.innerHTML += generateTodoHTML(inputValue)
   event.target.reset()
 })
 
